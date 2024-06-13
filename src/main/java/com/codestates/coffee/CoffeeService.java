05964d33cb3d6e1b04b896c65702f74c28063781
JinhyeongSeo <jh4clover@naver.com>
2024-06-13 10:27:05 +0900

    finish homework

---
+++ src/main/java/com/codestates/coffee/CoffeeService.java
@@ -0,0 +1,35 @@
+package com.codestates.coffee;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoffeeService {
+	public Coffee createCoffee(Coffee coffee) {
+		return coffee;
+	}
+
+	public Coffee updateCoffee(Coffee coffee) {
+		return coffee;
+	}
+
+	public Coffee findCoffee(long coffeeId) {
+		Coffee coffee = new Coffee(
+			coffeeId,"아메리카노","Americano",2500
+		);
+		return coffee;
+	}
+
+	public List<Coffee> findCoffees() {
+		List<Coffee> coffees = List.of(
+				new Coffee(1L,"아메리카노","Americano", 2500),
+				new Coffee(2L,"캐러멜 라떼","Caramel Latte",5000)
+		);
+		return coffees;
+	}
+
+	public void deleteCoffee(long coffeeId) {
+	}
+}
